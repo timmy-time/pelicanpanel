ee5bb463729dbdb35fb517c773438569752e59d5
notCharles <charles@pelican.dev>
2024-09-22 11:15:51 -0400

    Add Banners

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -18,6 +18,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Kenepa\Banner\BannerPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -68,6 +69,12 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                BannerPlugin::make()
+                    ->navigationIcon('tabler-pin')
+                    ->navigationLabel('Notices')
+                    ->navigationGroup('Advanced'),
             ]);
     }
 }
